69d7881b6d9995a7ca81268abc99cd3b3ccaa99d
makyo222 <145643799+makyo222@users.noreply.github.com>
2023-12-07 08:07:11 +0100

    Add files via upload

---
+++ ConsoleApp8/Alkatresz.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Alkatresz
+    {
+        string tipus;
+        string nev;
+        string parameter;
+        string ar;
+
+        public Alkatresz() { }
+        public Alkatresz(string tipus, string nev, string parameter, string ar)
+        {
+            this.tipus = tipus;
+            this.nev = nev;
+            this.parameter = parameter;
+            this.ar = ar;
+        }
+    }
+}
+
---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static List<Alkatresz> alkatreszek;
+        static void Beolvasas(string fajlNev)
+        {
+            alkatreszek = new List<Alkatresz>();
+            foreach (var sor in File.ReadAllLines(fajlNev))
+            {
+                var sorElemei = sor.Split(';');
+                string tipus = sorElemei[0];
+                string nev = sorElemei[1];
+                string parameter = sorElemei[2];
+                string ar = sorElemei[3];
+                alkatreszek.Add(new Alkatresz(tipus, nev, parameter, ar));
+            }
+        }
+        static void Kiirat (string fajlNev)
+        {
+            Console.Write("Bevitt alkatrészek törlése(igen/nem):");
+            string torles = Console.ReadLine();
+            if (torles == "igen")
+            {
+                Console.WriteLine("Az adatok törölve lettek!");
+            }
+            else if (torles == "nem")
+            {
+                Console.WriteLine("Az adatok NEM lettek törölve!");
+            }
+            else
+            {
+                Console.WriteLine("Nem lesznek törölve az adatok a helytelen válasz miatt!");
+            }
+        }
+        static void Main(string[] args)
+        {
+            
+            Console.ReadKey();
+        }
+    }
+}
