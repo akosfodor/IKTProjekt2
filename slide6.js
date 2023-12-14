66bf68ec4e44e75cc9a42017414bddcfeb7de2a6
akosfodor <144988738+akosfodor@users.noreply.github.com>
2023-12-14 10:08:38 +0100

    Add files via upload

---
+++ slide6.js
@@ -0,0 +1,21 @@
+let slideIndex6 = 1;
+showSlides6(slideIndex6);
+
+function plusSlides6(n) {
+  showSlides6(slideIndex6 += n);
+}
+
+function currentSlide6(n) {
+  showSlides6(slideIndex6 = n);
+}
+
+function showSlides6(n) {
+  let i;
+  let slides6 = document.getElementsByClassName("mySlides6");
+  if (n > slides6.length) {slideIndex6 = 1}
+  if (n < 1) {slideIndex6 = slides6.length}
+  for (i = 0; i < slides6.length; i++) {
+    slides6[i].style.display = "none";
+  }
+  slides6[slideIndex6-1].style.display = "block";
+}
